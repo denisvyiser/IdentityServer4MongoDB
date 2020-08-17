4c70021351281bf85989620db35b9b9e02425d59
Denis <denisvyiser@hotmail.com>
2020-08-17 18:59:31 -0300

    Inserido Configuração de Template e Atualizado query de consulta de Usuário

--- src/Project.IdentityServer.Application/Services/Identity/ResourceOwnerPasswordValidatorService.cs
+++ src/Project.IdentityServer.Application/Services/Identity/ResourceOwnerPasswordValidatorService.cs
@@ -31,7 +31,7 @@ namespace Project.identityserver.Application.Services
 
             FilterDefinition<User> filter = null;
 
-            filter = builder.Where(c => c.Username.ToUpper() == context.UserName & c.Password == context.Password);
+            filter = builder.Where(c => c.Username == context.UserName & c.Password == context.Password);
 
             var query = new GetUserQuery()
             {
